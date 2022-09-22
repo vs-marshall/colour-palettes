c99f9a178483f88a3412a5b7059c88700bd80893
lennox <78146861+vs-marshall@users.noreply.github.com>
2022-09-23 00:34:42 +0200

    Update fetchapi.js

--- scripts/fetchapi.js
+++ scripts/fetchapi.js
@@ -1,4 +1,4 @@
-$.getJSON('http://www.colourlovers.com/api/palettes/random?jsonCallback=?&numResults=1', function(data) {
+$.getJSON('https://www.colourlovers.com/api/palettes/random?jsonCallback=?&numResults=1', function(data) {
 
     var element = data[0];
 
